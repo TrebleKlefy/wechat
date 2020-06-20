300b661c13f3a0378a932d23eb82db90ac92ee0e
TrebleKlefy <shawakeem@outlook.com>
2020-06-20 17:20:39 -0500

    all files

---
+++ src/components/TextContainer/TextContainer.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import onlineIcon from '../../icons/onlineIcon.png';
+
+import './TextContainer.css';
+
+const TextContainer = ({ users }) => (
+  <div className="textContainer">
+    <div>
+      <h1>Yardi Chat <span role="img" aria-label="emoji">🇯🇲💬</span></h1>
+
+    </div>
+    {
+      users
+        ? (
+          <div>
+            <h2>who deh yah?</h2>
+            <div className="activeContainer">
+              <h3>
+                {users.map(({name}) => (
+                  <div key={name} className="activeItem">
+                    {name}
+                    <img alt="Online Icon" src={onlineIcon}/>
+                  </div>
+                ))}
+              </h3>
+            </div>
+          </div>
+        )
+        : null
+    }
+  </div>
+);
+
+export default TextContainer;
